efa02462533a6b3c4e342ffb6bcd049b94a78341
alexandre11aa <alexandrestrela@hotmail.com>
2024-11-15 18:35:55 -0300

    :sparkles: feat: Criando CRUD de usuários para API

--- api_freebible/user/admin.py
+++ api_freebible/user/admin.py
@@ -4,40 +4,35 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(BaseUserAdmin):
-
+    
     # Campos exibidos na lista de usuários
     list_display = (
-        'email', 'name', 'is_staff'
+        'id', 'email', 'nome', 'is_staff'
     )
     
     # Campos utilizados para pesquisa no painel administrativo
-    search_fields = ('email', 'name', 'nome_colaborador')
+    search_fields = ('email',)
     
     # Filtros disponíveis para filtrar usuários na lista
-    list_filter = ('is_staff', 'sexo', 'raca', 'estado_civil', 'necessidade_especial')
+    list_filter = ('is_staff', 'is_active')
     
     # Ordem de exibição dos usuários na lista
-    ordering = ('name',)
+    ordering = ('nome',)
     
     # Configurações para os formulários de edição e visualização
     fieldsets = (
         # Seção principal com campos básicos
-        (None, {'fields': ('email', 'name', 'password')}),
-        
-        # Seção de informações pessoais com diversos campos adicionais
-        ('Personal info', {
-            'fields': ('is_staff', 'is_active', 'last_login', 'sexo', 'raca', 'estado_civil', 'necessidade_especial', 'numero_tel', 'nacionalidade', 'nome_colaborador')
-        }),
-        
+        (None, {'fields': ('email', 'nome', 'password')}),
+                
         # Seção de permissões
-        ('Permissions', {'fields': ('is_superuser',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
     )
     
     # Campos a serem exibidos ao adicionar um novo usuário
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'name', 'password1', 'password2', 'is_staff', 'is_superuser', 'sexo', 'raca', 'estado_civil', 'necessidade_especial', 'numero_tel', 'nacionalidade', 'nome_colaborador')
+            'fields': ('email', 'nome', 'password1', 'password2', 'is_staff', 'is_superuser')
         }),
     )
     
